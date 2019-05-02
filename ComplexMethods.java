976294b813e8840d8f9bc542a73c96b07fb04fd4
Brandon Fergerson <fergerson92@gmail.com>
2019-05-01 21:41:41 -0600

    Create ComplexMethods.java

---
+++ ComplexMethods.java
@@ -0,0 +1,56 @@
+public class ComplexMethods {
+
+    void simpleMethod() {
+        if (true) {
+            System.out.println("");
+        }
+    }
+
+    void complexMethod() {
+        while (true) {
+            if (true) {
+                System.out.println("");
+            } else if (true) {
+                System.out.println("");
+            } else if (false) {
+                System.out.println("");
+            } else {
+                System.out.println("");
+            }
+            
+            switch (1) {
+            }
+        }
+    }
+
+    void mostComplexMethod() {
+        for (int i = 0; i < 10; i++) {
+            if (true) {
+                System.out.println("");
+            } else if (true) {
+                System.out.println("");
+            } else if (false) {
+                System.out.println("");
+            } else {
+                System.out.println("");
+                
+                if (true) {
+                    while (true) {
+                        if (true) {
+                            System.out.println("");
+                        } else if (true) {
+                            System.out.println("");
+                        } else if (false) {
+                            System.out.println("");
+                        } else {
+                            System.out.println("");
+                        }
+                    }
+                }
+            }
+            
+            switch (1) {
+            }
+        }
+    }
+}
